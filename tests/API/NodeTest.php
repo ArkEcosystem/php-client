f9d1dae606769b4f2fae09244f5721cc50d4d6f9
Edgar Goetzendorff <hello@dated.fun>
2019-09-03 04:35:03 +0200

    feat: crypto endpoint (#63)

--- tests/API/NodeTest.php
+++ tests/API/NodeTest.php
@@ -48,6 +48,14 @@ class NodeTest extends TestCase
     }
 
     /** @test */
+    public function crypto_calls_correct_url()
+    {
+        $this->assertResponse('GET', 'node/configuration/crypto', function ($connection) {
+            return $connection->node()->crypto();
+        });
+    }
+
+    /** @test */
     public function fees_calls_correct_url()
     {
         $this->assertResponse('GET', 'node/fees', function ($connection) {
