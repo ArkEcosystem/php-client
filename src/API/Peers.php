5b37ac282c415275bf6961d16769d24e407706e3
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-08-01 09:21:07 -0600

    remove author and disclaimer

--- src/API/Peers.php
+++ src/API/Peers.php
@@ -2,22 +2,8 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Ark PHP Client.
- *
- * (c) Ark Ecosystem <info@ark.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ArkEcosystem\Client\API;
 
-/**
- * This is the peers resource class.
- *
- * @author Brian Faust <brian@ark.io>
- */
 class Peers extends AbstractAPI
 {
     /**
--- src/Connection.php
+++ src/Connection.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Ark PHP Client.
- *
- * (c) Ark Ecosystem <info@ark.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ArkEcosystem\Client;
 
 use BadMethodCallException;
@@ -19,11 +10,6 @@ use GuzzleHttp\HandlerStack;
 use Illuminate\Support\Str;
 use RuntimeException;
 
-/**
- * This is the connection class.
- *
- * @author Brian Faust <brian@ark.io>
- */
 class Connection
 {
     /**
