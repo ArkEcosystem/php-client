73e34861641575266465adf9735adbb20441a115
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-08-05 09:53:53 -0600

    remove more dockblocks

--- src/ClientManager.php
+++ src/ClientManager.php
@@ -2,23 +2,12 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Ark PHP Client.
- *
- * (c) Ark Ecosystem <info@ark.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ArkEcosystem\Client;
 
 use InvalidArgumentException;
 
 /**
  * This is the client manager class.
- *
- * @author Brian Faust <brian@ark.io>
  */
 class ClientManager
 {
