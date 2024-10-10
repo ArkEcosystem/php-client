35b4b5b12ef3a136ccc16cdcefc4bdf3d1bfdc47
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-10-10 13:01:09 -0600

    add receipts handling

---
+++ src/API/Receipts.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Client\API;
+
+use Exception;
+
+class Receipts extends AbstractAPI
+{
+    /**
+     * Get all receipts (paginated).
+     *
+     * @param array $query
+     *
+     * @return array
+     */
+    public function all(array $query = []): ?array
+    {
+        return $this->get('receipts', $query);
+    }
+
+    /**
+     * Get a receipt by the given transaction hash.
+     *
+     * @param string $txHash
+     *
+     * @return array
+     */
+    public function show(string $txHash): ?array
+    {
+        $result = $this->get("receipts", ['txHash' => $txHash])['data'];
+
+        if (empty($result)) {
+            throw new Exception(sprintf('No receipt found for transaction %s', $txHash));
+        }
+
+        return $result[0];
+    }
+}
