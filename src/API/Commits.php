a2e4689c9e308d09abbb3b670ea1cf38c603547b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-17 12:21:30 +0100

    update height argument for commit block number

--- src/API/Commits.php
+++ src/API/Commits.php
@@ -7,14 +7,14 @@ namespace ArkEcosystem\Client\API;
 class Commits extends AbstractAPI
 {
     /**
-     * Takes a block height and returns the commit details.
+     * Takes a block number and returns the commit details.
      *
      * @param int $id
      *
      * @return array
      */
-    public function get(int $height): ?array
+    public function get(int $number): ?array
     {
-        return $this->requestGet("commits/{$height}");
+        return $this->requestGet("commits/{$number}");
     }
 }
